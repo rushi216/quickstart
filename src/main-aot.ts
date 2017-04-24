71f732b52b64435805a6542a4fea5f145297799a
rushi <rushi_216@yahoo.com>
2017-04-24 11:58:41 +0530

    added aot with systemjs

---
+++ src/main-aot.ts
@@ -0,0 +1,4 @@
+﻿import { platformBrowser }    from '@angular/platform-browser';
+import { AppModuleNgFactory } from './aot/app/app.module.ngfactory';
+console.log('Running AOT compiled');
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
